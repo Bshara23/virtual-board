0713d8be8d7f5997b0a62284250243c706615ea5
unknown <bshara23@gmail.com>
2020-12-03 10:04:37 +0200

    added camera feed

---
+++ Assets/CameraListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraListener : MonoBehaviour
+{
+    WebCamTexture webCamTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        var renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
