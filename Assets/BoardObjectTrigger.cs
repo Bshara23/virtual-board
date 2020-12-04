78141245bcb3784b5e8917873c9271f1e91e1305
unknown <bshara23@gmail.com>
2020-12-05 00:53:31 +0200

    more gestures

---
+++ Assets/BoardObjectTrigger.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoardObjectTrigger : MonoBehaviour
+{
+    #region Private Props
+    bool runOnce = false;
+
+    public float ZPos;
+    Vector3 Offset;
+    bool Dragging;
+    #endregion
+
+    #region Inspector Variables
+    public Camera MainCamera;
+    [Space]
+    [SerializeField]
+    public UnityEvent OnBeginDrag;
+    [SerializeField]
+    public UnityEvent OnEndDrag;
+    #endregion
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!runOnce)
+        {
+            string tag = "HandKeyPoint";
+            print(other.tag);
+            runOnce = !runOnce;
+        }
+        
+    }
+    void Start()
+    {
+        MainCamera = Camera.main;
+        ZPos = MainCamera.WorldToScreenPoint(transform.position).z;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (MLClient.hand == null) return;
+
+        string rg = MLClient.hand.hg.rightGesture;
+        if (rg == "pinch")
+        {
+            if (!Dragging)
+                BeginDrag();
+        }
+        else
+        {
+            if (rg == "open")
+            {
+                EndDrag();
+            }
+        }
+    }
+    void Update()
+    {
+        if (Dragging)
+        {
+
+            float px = MLClient.mainTransform.localScale.x * 10 * MLClient.hand.hg.right[8].x - MLClient.mainTransform.localScale.x * 5;
+            float py = MLClient.mainTransform.localScale.z * 10 * MLClient.hand.hg.right[8].y - MLClient.mainTransform.localScale.z * 5;
+            
+            Vector3 pos = new Vector3(px, py, transform.position.z);
+            transform.position = Offset + pos;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        runOnce = true;
+
+    }
+
+    private void EndDrag()
+    {
+        OnEndDrag.Invoke();
+        Dragging = false;
+    }
+
+    public void BeginDrag()
+    {
+        OnBeginDrag.Invoke();
+        Dragging = true;
+
+        if (MLClient.hand.hg.rightGesture.Length > 0)
+        {
+
+            float px = MLClient.mainTransform.localScale.x * 10 * MLClient.hand.hg.right[8].x - MLClient.mainTransform.localScale.x * 5;
+            float py = MLClient.mainTransform.localScale.z * 10 * MLClient.hand.hg.right[8].y - MLClient.mainTransform.localScale.z * 5;
+
+            Offset = transform.position - new Vector3(px, py, 0);
+
+        }
+    }
+}
--- Assets/MLClient.cs
+++ Assets/MLClient.cs
@@ -21,23 +21,43 @@ public class MLClient : MonoBehaviour
     public GameObject spherePrefab;
     private GameObject[] spheresLeft;
     private GameObject[] spheresRight;
-
-    public Transform note;
+    public GameObject content;
     private Vector3 originalPos;
+    private GameObject rightPointer;
+    public GameObject rightPointerPrefab;
+    private GameObject leftPointer;
+    public GameObject leftPointerPrefab;
+    public GameObject lockIndicator;
     public float speed = 55;
     public float leng = 10f;
+    private float lockTimer = 0;
+    private bool gLock = false;
+    public float globalScroll = 10f;
+    public Vector3 ptrPos;
+    public static Hand hand;
+    public static Transform mainTransform;
     void Start()
     {
         s.Connect(ep);
+        mainTransform = transform;
 
         spheresLeft = new GameObject[21];
         spheresRight = new GameObject[21];
         for (int i = 0; i < 21; i++)
         {
             spheresLeft[i] = Instantiate(spherePrefab, transform);
+            spheresLeft[i].SetActive(false);
             spheresRight[i] = Instantiate(spherePrefab, transform);
+            spheresRight[i].SetActive(false);
         }
+
+        rightPointer = Instantiate(rightPointerPrefab, transform);
+        leftPointer = Instantiate(leftPointerPrefab, transform);
+        rightPointer.SetActive(false);
+        leftPointer.SetActive(false);
     }
+
+
 
 
     void Update()
@@ -48,100 +68,237 @@ public class MLClient : MonoBehaviour
             int nbytes = s.Receive(buffer);
             string res = Encoding.UTF8.GetString(buffer, 0, nbytes);
 
-            Hand hand = new Hand(res);
+            hand = new Hand(res);
 
 
-            if (hand.hg.right.Count > 0)
+
+            if (hand.hg.right.Count > 0 && hand.hg.left.Count > 0)
             {
-
-                for (int i = 0; i < 21; i++)
-                {
-                    float px = transform.localScale.x * 10 * hand.hg.right[i].x - transform.localScale.x * 5;
-                    float py = transform.localScale.z * 10 * hand.hg.right[i].y - transform.localScale.z * 5;
-                    Vector3 pos = new Vector3(px, py, 0);
-                    spheresRight[i].SetActive(true);
-                    spheresRight[i].transform.position = transform.position + pos;
-                }
-
-                if (hand.hg.rightGesture == "pinch")
-                {
-                    print("right pinch");
-                    note.localScale = new Vector3(-5, 5, 0);
-                }
-                else if (hand.hg.rightGesture == "open")
-                {
-                    print("right open");
-                    note.localScale = new Vector3(-3, 3, 0);
-                }
-                else if (hand.hg.rightGesture == "closed")
-                {
-                    print("right closed");
-                    note.localScale = new Vector3(-5, 5, 0);
-
-                    note.position = new Vector3((float)Math.Sin(Time.time) * leng + originalPos.x + transform.position.x, originalPos.y + transform.position.y, originalPos.z + transform.position.z);
-                }
-                else if (hand.hg.rightGesture == "point")
-                {
-                    print("right point");
-                }
-
-
+                BothHandsHandler(hand);
             }
-            else
+            if (gLock == false)
             {
-                for (int i = 0; i < 21; i++)
+                if (hand.hg.right.Count > 0)
                 {
-                    spheresRight[i].SetActive(false);
+
+                    RightHandHandler(hand);
+                    for (int i = 8; i < 9; i++)
+                    {
+                        spheresLeft[i].SetActive(true);
+                    }
+
+                }
+
+                else if (hand.hg.left.Count > 0)
+                {
+
+                    LeftHandHandler(hand);
+                    for (int i = 0; i < 21; i++)
+                    {
+                        spheresRight[i].SetActive(true);
+                    }
+
                 }
             }
 
-            if (hand.hg.left.Count > 0)
-            {
 
-                for (int i = 0; i < 21; i++)
-                {
-                    float px = transform.localScale.x * 10 * hand.hg.left[i].x - transform.localScale.x * 5;
-                    float py = transform.localScale.z * 10 * hand.hg.left[i].y - transform.localScale.z * 5;
-                    Vector3 pos = new Vector3(px, py, 0);
-                    spheresLeft[i].SetActive(true);
-                    spheresLeft[i].transform.position = transform.position + pos;
-                }
-
-                if (hand.hg.leftGesture == "pinch")
-                {
-                    print("left pinch");
-                }
-                else if (hand.hg.leftGesture == "open")
-                {
-                    print("left open");
-                }
-                else if (hand.hg.leftGesture == "closed")
-                {
-                    print("left closed");
-
-                }
-                else if (hand.hg.leftGesture == "point")
-                {
-                    print("left point");
-                }
-
-
-            }
+            
             else
             {
                 for (int i = 0; i < 21; i++)
                 {
                     spheresLeft[i].SetActive(false);
+                    spheresRight[i].SetActive(false);
                 }
+                rightPointer.SetActive(false);
+                leftPointer.SetActive(false);
+
             }
+
         }
         catch (SocketException e)
         {
-            print(e.Message + e.ErrorCode);
+            //print(e.Message + e.ErrorCode);
+            rightPointer.SetActive(false);
+            leftPointer.SetActive(false);
         }
         catch (Exception e)
         {
-            print(e.Message);
+            //print(e.Message);
+            rightPointer.SetActive(false);
+            leftPointer.SetActive(false);
         }
+    }
+
+
+
+    private void BothHandsHandler(Hand hand)
+    {
+        rightPointer.SetActive(false);
+        leftPointer.SetActive(false);
+
+      
+
+        if (gLock == false)
+        {
+
+            if (hand.hg.leftGesture == "closed" && hand.hg.rightGesture == "closed")
+            {
+                DisableGesturesHandler();
+                //print("both closed");
+            }
+            if (hand.hg.leftGesture == "open" && hand.hg.rightGesture == "closed")
+            {
+                OpacityHandler();
+                //print("open-closed");
+
+            }
+            else if (hand.hg.leftGesture == "pinch" && hand.hg.rightGesture == "closed")
+            {
+                RotateObjectHandler();
+                //print("pinch-closed");
+
+            }
+            else if (hand.hg.leftGesture == "pinch" && hand.hg.rightGesture == "pinch")
+            {
+                ScaleObjectHandler();
+                //print("pinch-pinch");
+
+            }
+        }
+        else
+        {
+            if (hand.hg.leftGesture == "open" && hand.hg.rightGesture == "open")
+            {
+                EnableGestureHandler();
+            }
+        }
+        
+
+    }
+
+
+    private void ScaleObjectHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RotateObjectHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OpacityHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EnableGestureHandler()
+    {
+        gLock = false;
+        lockIndicator.SetActive(false);
+    }
+    private void DisableGesturesHandler()
+    {
+        gLock = true;
+        lockIndicator.SetActive(true);
+
+    }
+
+    private void RightHandHandler(Hand hand)
+    {
+        for (int i = 0; i < 21; i++)
+        {
+            float px = transform.localScale.x * 10 * hand.hg.right[i].x - transform.localScale.x * 5;
+            float py = transform.localScale.z * 10 * hand.hg.right[i].y - transform.localScale.z * 5;
+            Vector3 pos = new Vector3(px, py, 0);
+            spheresRight[i].SetActive(true);
+            spheresRight[i].transform.position = transform.position + pos;
+        }
+
+        if (hand.hg.rightGesture == "pinch")
+        {
+            //print("right pinch");
+            rightPointer.SetActive(false);
+        }
+        else if (hand.hg.rightGesture == "open")
+        {
+            //print("right open");
+            rightPointer.SetActive(false);
+
+        }
+        else if (hand.hg.rightGesture == "closed")
+        {
+            //print("right closed");
+            content.transform.position = content.transform.position + RightPointMapped(hand, 8) * Time.deltaTime * globalScroll;
+            rightPointer.SetActive(false);
+
+        }
+        else if (hand.hg.rightGesture == "point")
+        {
+            //print("right point");
+            rightPointer.SetActive(true);
+            leftPointer.SetActive(false);
+            rightPointer.transform.position = transform.position + RightPointMapped(hand, 8) + ptrPos;
+
+        }
+    }
+
+    private void LeftHandHandler(Hand hand)
+    {
+        for (int i = 8; i < 9; i++)
+        {
+            float px = transform.localScale.x * 10 * hand.hg.left[i].x - transform.localScale.x * 5;
+            float py = transform.localScale.z * 10 * hand.hg.left[i].y - transform.localScale.z * 5;
+            Vector3 pos = new Vector3(px, py, 0);
+            spheresLeft[i].SetActive(true);
+            spheresLeft[i].transform.position = transform.position + pos;
+        }
+
+        if (hand.hg.leftGesture == "pinch")
+        {
+            //print("left pinch");
+            leftPointer.SetActive(false);
+
+        }
+        else if (hand.hg.leftGesture == "open")
+        {
+            //print("left open");
+            leftPointer.SetActive(false);
+
+        }
+        else if (hand.hg.leftGesture == "closed")
+        {
+            //print("left closed");
+            leftPointer.SetActive(false);
+
+
+        }
+        else if (hand.hg.leftGesture == "point")
+        {
+            //print("left point");
+            leftPointer.SetActive(true);
+            rightPointer.SetActive(false);
+
+            leftPointer.transform.position = transform.position + LeftPointMapped(hand, 8) + ptrPos; 
+
+        }
+
+    }
+
+    private Vector3 LeftPointMapped(Hand hand, int i)
+    {
+        float px = transform.localScale.x * 10 * hand.hg.left[i].x - transform.localScale.x * 5;
+        float py = transform.localScale.z * 10 * hand.hg.left[i].y - transform.localScale.z * 5;
+        Vector3 pos = new Vector3(px, py, 0);
+        return pos;
+    }
+
+    private Vector3 RightPointMapped(Hand hand, int i)
+    {
+        float px = transform.localScale.x * 10 * hand.hg.right[i].x - transform.localScale.x * 5;
+        float py = transform.localScale.z * 10 * hand.hg.right[i].y - transform.localScale.z * 5;
+        Vector3 pos = new Vector3(px, py, 0);
+        return pos;
     }
 }
